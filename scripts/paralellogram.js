f9f841c92f7fc8e5ee6172a4950a9c3fd0140638
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-25 21:50:12 +0600

    commited

--- scripts/paralellogram.js
+++ scripts/paralellogram.js
@@ -1,65 +1,22 @@
-
-
-
-// console.log('connected paralellogram')
-
-// function paralellogramArea(){
-//     // const  baseInput = document.getElementById('input-base');
-//     // const baseText = baseInput.value;
-//     // const base = parseFloat(baseText);
-//     const base = getParalellogramBase();
-//     console.log(base);
-// }
-
-// function getParalellogramBase(){
-//     const  baseInput = document.getElementById();
-//     const baseText = baseInput.value;
-//     const base = parseFloat(baseText);
-//     return base;
-// }
-
-
-
-
-
-
-
 function paralellogramArea(){
     const base = getInputValueById('input-base');
-    // console.log('base value', base);
-
+    console.log(base);
     const height = getInputValueById('input-height');
-    // console.log('base value', height);
+    console.log(height);
 
     const area = base* height;
     console.log(area);
-    setInnerText('paralellogram-area', area)
-
-
-
+    setInnerTextById('paralellogram-area')
 }
 
 function getInputValueById(inputFieldId){
     const inputField = document.getElementById(inputFieldId);
-    const inputValueText = inputField.value;
-    const inputValue = parseFloat(inputValueText);
-    // console.log(inputValue);
-    return inputValue;
-
+   const inputText = inputField.value;
+   const text = parseFloat(inputText)
+    return text;
 }
 
-function setInnerText(elementId, area){
+function setInnerTextById(elementId, area){
     const element = document.getElementById(elementId);
     element.innerText = area;
-} 
-
-
-
-
-
-
-
-
-
-
-
+}
