a177135f06a75a7077cdf5acaa74bfc41d765d4e
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-25 19:35:56 +0600

    commited

--- scripts/paralellogram.js
+++ scripts/paralellogram.js
@@ -1,1 +1,16 @@
 console.log('connected paralellogram')
+
+function paralellogramArea(){
+    // const  baseInput = document.getElementById('input-base');
+    // const baseText = baseInput.value;
+    // const base = parseFloat(baseText);
+    const base = getParalellogramBase();
+    console.log(base);
+}
+
+function getParalellogramBase(){
+    const  baseInput = document.getElementById('input-base');
+    const baseText = baseInput.value;
+    const base = parseFloat(baseText);
+    return base;
+}
