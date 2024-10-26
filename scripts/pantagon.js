c7164f98f9a3b6a775c5d0d3090c5e6b44423405
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-26 09:23:44 +0600

    commited

--- scripts/pantagon.js
+++ scripts/pantagon.js
@@ -1,3 +1,23 @@
 function pentagonArea(){
-    
+    const perimeter = getInputValue('pentagon-perimeter');
+    const apothem = getInputValue('pentagon-apothem');
+    console.log(perimeter, apothem);
+
+    const area = 0.5 * perimeter * apothem;
+    setInnerText('pentagon-span', area)
+
+}
+
+function getInputValue(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputText = inputField.value;
+    const value = parseFloat(inputText);
+    return value;
+
+}
+
+function setInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+
 }
--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -1,21 +1,21 @@
-function rectanglaArea (){
-    console.log('rectangle area');
-    const lengthInput = document.getElementById('rectangle-width');
-    const lengthText = lengthInput.value;
-    const length = parseFloat(lengthText);
-    console.log(length);
+// function rectanglaArea (){
+//     console.log('rectangle area');
+//     const lengthInput = document.getElementById('rectangle-width');
+//     const lengthText = lengthInput.value;
+//     const length = parseFloat(lengthText);
+//     console.log(length);
 
-    //get width of the rectangle
+//     //get width of the rectangle
 
-    const widthInput = document.getElementById('rectangle-length');
-    const widthText = widthInput.value;
-    const width = parseFloat(widthText);
-    console.log(width);
+//     const widthInput = document.getElementById('rectangle-length');
+//     const widthText = widthInput.value;
+//     const width = parseFloat(widthText);
+//     console.log(width);
 
-    const area = length * width;
-    console.log("rectangle area",area);
+//     const area = length * width;
+//     console.log("rectangle area",area);
 
 
-    const rectangleAreaSpan = document.getElementById('rectangle-area');
-    rectangleAreaSpan.innerText = area;
-}
+//     const rectangleAreaSpan = document.getElementById('rectangle-area');
+//     rectangleAreaSpan.innerText = area;
+// }
